395731c68cbb9839f59fecb5d8855425a384fc8d
Anoia <github@stuckinadrawer.com>
2024-04-12 22:07:39 +0200

    add mantine

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import { Roboto_Flex } from "next/font/google";
 import "./globals.css";
 import Nav from "./_components/nav";
 import Footer from "./_components/footer";
 
-const roboto = Roboto_Flex({ subsets: ["latin"] });
+import "@mantine/core/styles.css";
 
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { theme } from "@/theme";
 export const metadata: Metadata = {
   title: "recipe-drawer",
   description: "recipes by eddy and enno",
@@ -18,14 +19,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
-        <div className="font-extralight flex flex-col h-screen w-screen">
-          <Nav />
-          <main className="flex-grow grid grid-cols-1 container mx-auto max-w-4xl">
-            {children}
-          </main>
-          <Footer />
-        </div>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>
+          <div className="flex flex-col h-screen w-screen">
+            <Nav />
+            <main className="flex-grow grid grid-cols-1 container mx-auto max-w-4xl">
+              {children}
+            </main>
+            <Footer />
+          </div>
+        </MantineProvider>
       </body>
     </html>
   );
