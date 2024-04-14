1158cfa5be57eb47be46964ff3d0a1eb64e8f840
Anoia <github@stuckinadrawer.com>
2024-04-14 11:43:52 +0200

    switch ingredient creation to mantine

--- app/ingredients/createIngredientComponent.tsx
+++ app/ingredients/createIngredientComponent.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
-import { StandardButton } from "../_components/standardComponents/styledcomponents";
-import { Button } from "@mantine/core";
+import React, { useEffect, useState } from "react";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
+import { Modal, Button, TextInput, Radio, Flex } from "@mantine/core";
 import DietDisplay from "../_components/standardComponents/diet";
 import { createIngredient } from "./actions";
 import { useFormState, useFormStatus } from "react-dom";
@@ -12,39 +12,21 @@ const initialState = {
   status: "",
 };
 
-function SubmitButton() {
-  const { pending } = useFormStatus();
-
-  return (
-    <Button type="submit" disabled={pending}>
-      Create
-    </Button>
-  );
-}
-
-export default function CreateIngredientDialog(props: {
-  isOpen: boolean;
+export default function CreateIngredientForm(props: {
+  opened: boolean;
   input: string;
   close: () => void;
 }) {
   const [newIngredientName, setNewIngredientName] = useState(props.input);
   const [diet, setDiet] = useState("Vegan");
-  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    inputRef.current?.focus();
     setDiet("Vegan");
     setNewIngredientName(props.input);
-  }, [props.isOpen, props.input]);
-
-  const handleKey = (e: React.KeyboardEvent<HTMLElement>) => {
-    if (e.key == "Escape") {
-      e.preventDefault();
-      props.close();
-    }
-  };
+  }, [props.opened, props.input]);
 
   const [state, formAction] = useFormState(createIngredient, initialState);
+  const { pending } = useFormStatus();
 
   useEffect(() => {
     if (state.status == "Success") {
@@ -58,123 +40,74 @@ export default function CreateIngredientDialog(props: {
   }, [state, props]);
 
   return (
-    <dialog
-      open={props.isOpen}
-      className="fixed inset-x-0 top-48 z-10 overflow-y-auto p-0 w-1/4 min-w-fit"
-      onKeyUp={(e) => handleKey(e)}
-      tabIndex={-1}
-    >
-      <div>
-        <div
-          className="fixed inset-0 bg-black opacity-30"
-          onClick={() => props.close()}
-        />
-        <form
-          id="create-ingredient-form"
-          action={formAction}
-          className="relative w-full mx-auto bg-white rounded p-10 flex flex-col"
+    <>
+      <form id="create-ingredient-form" action={formAction}>
+        <TextInput
+          data-autofocus
+          label="Name der Zutat"
+          value={newIngredientName}
+          name="ingredientName"
+          onChange={(e) => setNewIngredientName(e.target.value)}
+        ></TextInput>
+
+        <Radio.Group
+          mt="sm"
+          name="diet"
+          label="Wähle die Diät der Zutat:"
+          value={diet}
+          onChange={setDiet}
         >
-          <h1 className="mb-5 text-4xl">Create new Ingredient</h1>
-          <p className="my-1">Please enter the ingredient name:</p>
-          <input
-            className="mb-5 border p-2"
-            type="text"
-            ref={inputRef}
-            value={newIngredientName}
-            name="ingredientName"
-            onChange={(e) => setNewIngredientName(e.target.value)}
-          ></input>
-
-          <div>
-            <div className="flex justify-between">
-              <div className="flex items-center mx-2">
-                <input
-                  type="radio"
-                  id="Vegan"
-                  name="diet"
-                  value="Vegan"
-                  checked={diet === "Vegan"}
-                  onChange={(e) => setDiet(e.target.value)}
-                  className="w-4 h-4"
-                />
-                <label htmlFor="Vegan" className="ml-2">
-                  <DietDisplay diet="Vegan" />
-                </label>
-              </div>
-
-              <div className="flex items-center mx-2">
-                <input
-                  type="radio"
-                  id="Vegetarian"
-                  name="diet"
-                  value="Vegetarian"
-                  checked={diet === "Vegetarian"}
-                  onChange={(e) => setDiet(e.target.value)}
-                  className="w-4 h-4"
-                />
-                <label htmlFor="Vegetarian" className="ml-2">
-                  <DietDisplay diet="Vegetarian" />
-                </label>
-              </div>
-
-              <div className="flex items-center  mx-2">
-                <input
-                  type="radio"
-                  id="Fish"
-                  name="diet"
-                  value="Fish"
-                  checked={diet === "Fish"}
-                  onChange={(e) => setDiet(e.target.value)}
-                  className="w-4 h-4"
-                />
-                <label htmlFor="Fish" className="ml-2">
-                  <DietDisplay diet="Fish" />
-                </label>
-              </div>
-
-              <div className="flex items-center mx-2">
-                <input
-                  type="radio"
-                  id="Meat"
-                  name="diet"
-                  value="Meat"
-                  checked={diet === "Meat"}
-                  onChange={(e) => setDiet(e.target.value)}
-                  className="w-4 h-4"
-                />
-                <label htmlFor="Meat" className="ml-2">
-                  <DietDisplay diet="Meat" />
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse mt-12 space-x-5 space-x-reverse">
-            <SubmitButton />
-            <Button
-              variant="outline"
-              type="button"
-              onClick={() => props.close()}
-            >
-              Cancel
-            </Button>
-          </div>
-        </form>
-      </div>
-    </dialog>
+          <Flex direction={{ base: "column", sm: "row" }} gap={{ base: "sm" }}>
+            <Radio
+              value="Vegan"
+              label={<DietDisplay diet="Vegan" />}
+              classNames={{ label: "pl-1" }}
+            />
+            <Radio
+              value="Vegetarian"
+              label={<DietDisplay diet="Vegetarian" />}
+              classNames={{ label: "pl-1" }}
+            />
+            <Radio
+              value="Fish"
+              label={<DietDisplay diet="Fish" />}
+              classNames={{ label: "pl-1" }}
+            />
+            <Radio
+              value="Meat"
+              label={<DietDisplay diet="Meat" />}
+              classNames={{ label: "pl-1" }}
+            />
+          </Flex>
+        </Radio.Group>
+        <div className="flex flex-row-reverse mt-12 space-x-5 space-x-reverse">
+          <Button type="submit" disabled={pending}>
+            Erstellen
+          </Button>
+          <Button variant="outline" type="button" onClick={() => props.close()}>
+            Abbrechen
+          </Button>
+        </div>
+      </form>
+    </>
   );
 }
 
 export function CreateIngredientButton() {
-  const [dialogOpen, setDialogOpen] = useState(false);
-
+  const [opened, { open, close }] = useDisclosure(false);
+  const isMobile = useMediaQuery("(max-width: 50em)");
   return (
     <>
-      <Button onClick={() => setDialogOpen(true)}>Neue Zutat</Button>
-      <CreateIngredientDialog
-        isOpen={dialogOpen}
-        input={""}
-        close={() => setDialogOpen(false)}
-      />
+      <Button onClick={open}>Neue Zutat</Button>
+      <Modal
+        fullScreen={isMobile}
+        size="auto"
+        opened={opened}
+        onClose={close}
+        title="Neue Zutat anlegen"
+      >
+        <CreateIngredientForm opened={opened} input={""} close={close} />
+      </Modal>
     </>
   );
 }
