6d88361b536eb4a3b9675e9a7dfd8f21d3b03776
Anoia <github@stuckinadrawer.com>
2024-04-28 16:24:17 +0200

    add autocomplete input parsing

--- app/_components/ingredientAutocomplete.tsx
+++ app/_components/ingredientAutocomplete.tsx
@@ -1,33 +1,106 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Combobox, InputBase, Modal, useCombobox } from "@mantine/core";
 import { $default } from "@/dbschema/interfaces";
 import Ingredient = $default.Ingredient;
 import Unit = $default.Unit;
 import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import CreateIngredientForm from "@/app/ingredients/createIngredientComponent";
+import { extractRecipeMatchResult, Maybe } from "@/app/_stuff/regexParser";
+
+export type IngredientSelection = {
+  amount: number;
+  unit: Maybe<Unit>;
+  ingredientString: string;
+  extraInfo: Maybe<string>;
+};
 
 export function IngredientAutocomplete(props: {
   ingredients: Ingredient[];
   units: Unit[];
   onSelectIngredient: (ingredient: Ingredient) => void;
 }) {
+  const [matchResult, setMatchResult] =
+    useState<Maybe<IngredientSelection>>(undefined);
+
+  const [userInputString, setUserInputString] = useState("");
+
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
   });
 
-  const [search, setSearch] = useState("");
-
-  const filteredOptions = props.ingredients.filter((item) =>
-    item.name.toLowerCase().includes(search.toLowerCase().trim()),
+  const [fuzzyIngredients, setFuzzyIngredients] = useState<Array<Ingredient>>(
+    props.ingredients,
   );
 
-  const options = filteredOptions.map((item) => (
-    <Combobox.Option value={item.name} key={item.id}>
-      {item.name} - {item.diet}
-    </Combobox.Option>
-  ));
+  useEffect(() => {
+    const r = props.ingredients.filter((item) =>
+      item.name
+        .toLowerCase()
+        .includes((matchResult?.ingredientString ?? "").toLowerCase().trim()),
+    );
+    setFuzzyIngredients(r);
+  }, [props.ingredients, matchResult]);
+
+  useEffect(() => {
+    function findExactUnitMatch(unitName: string): Maybe<Unit> {
+      return props.units.find(
+        (u) =>
+          u.long_name.toLowerCase() === unitName.toLowerCase() ||
+          u.short_name.toLowerCase() === unitName.toLowerCase(),
+      );
+    }
+
+    const match = extractRecipeMatchResult(userInputString);
+
+    if (match) {
+      const possibleUnit = match.unitName
+        ? findExactUnitMatch(match.unitName)
+        : undefined;
+
+      const rawIngredientString = match.ingredientName.trim();
+      const ingredientString =
+        match.unitName != undefined &&
+        possibleUnit == undefined &&
+        rawIngredientString
+          ? `${match.unitName} ${rawIngredientString}`
+          : rawIngredientString;
+
+      const result: IngredientSelection = {
+        amount: match.amount,
+        unit: possibleUnit,
+        ingredientString: ingredientString,
+        extraInfo: match.extra_info,
+      };
+
+      setMatchResult(result);
+    } else {
+      setMatchResult(undefined);
+    }
+  }, [userInputString, props.units]);
+
+  function escapeReg(i: string) {
+    return i.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
+
+  function findPossibleUnits(unitName: string): Unit[] {
+    const regexp = new RegExp(escapeReg(unitName.trim()), "i");
+    return props.units.filter(
+      (u) => u.long_name.match(regexp) || u.short_name.match(regexp),
+    );
+  }
+  function displayDropdown(): boolean {
+    const unsureAboutInput =
+      matchResult?.unit == undefined &&
+      matchResult?.ingredientString != undefined &&
+      findPossibleUnits(matchResult.ingredientString).length > 0;
+    return (
+      !unsureAboutInput &&
+      matchResult?.ingredientString != undefined &&
+      matchResult?.ingredientString?.length >= 2
+    );
+  }
 
   // for create ingredient modal
   const [opened, { open, close }] = useDisclosure(false);
@@ -50,7 +123,7 @@ export function IngredientAutocomplete(props: {
           } else {
             if (selectedOption) {
               props.onSelectIngredient(selectedOption);
-              setSearch("");
+              setUserInputString("");
               combobox.closeDropdown();
             }
           }
@@ -59,13 +132,12 @@ export function IngredientAutocomplete(props: {
         <Combobox.Target>
           <InputBase
             // rightSection={<Combobox.ClearButton />}
-            value={search}
+            value={userInputString}
             onChange={(event) => {
-              console.log("change");
               combobox.openDropdown();
               combobox.updateSelectedOptionIndex();
               if (event.currentTarget.value !== "$create") {
-                setSearch(event.currentTarget.value);
+                setUserInputString(event.currentTarget.value);
               }
             }}
             onClick={() => combobox.openDropdown()}
@@ -76,20 +148,24 @@ export function IngredientAutocomplete(props: {
                 console.log("creating new ingredient");
               } else {
                 combobox.closeDropdown();
-                setSearch("");
+                setUserInputString("");
               }
             }}
-            placeholder="Search value"
+            placeholder="100g Mehl"
             rightSectionPointerEvents="none"
           />
         </Combobox.Target>
 
-        <Combobox.Dropdown hidden={search.length < 2}>
+        <Combobox.Dropdown hidden={!displayDropdown()}>
           <Combobox.Options>
-            {options}
-            {search.trim().length > 0 && (
+            {fuzzyIngredients.map((item) => (
+              <Combobox.Option value={item.name} key={item.id}>
+                {item.name} - {item.diet}
+              </Combobox.Option>
+            ))}
+            {(matchResult?.ingredientString ?? "").trim().length > 0 && (
               <Combobox.Option value="$create">
-                + Create {search}
+                + Create {matchResult?.ingredientString ?? ""}
               </Combobox.Option>
             )}
           </Combobox.Options>
@@ -103,7 +179,11 @@ export function IngredientAutocomplete(props: {
         title="Neue Zutat anlegen"
         zIndex={999} // ensure it is displayed over open combobox
       >
-        <CreateIngredientForm opened={opened} input={search} close={close} />
+        <CreateIngredientForm
+          opened={opened}
+          input={matchResult?.ingredientString ?? ""}
+          close={close}
+        />
       </Modal>
     </>
   );
